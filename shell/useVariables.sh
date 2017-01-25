af9e8b210b26bc5613357396b8ebf137bbf1a9fe
高曾荣 <zengrong.gzr@alibaba-inc.com>
2017-01-25 10:36:27 +0800

    use variables

---
+++ shell/useVariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# testing variables
+# 如何在shell脚本中使用变量 https://linux.cn/article-3834-1.html
+days=10
+guest="Katie"
+echo "$guest checked in $days days ago"
+days=5
+guest="Jessica"
+echo "$guest checked in $days days ago"
